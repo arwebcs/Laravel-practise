0c8de66b5b7ee4707664f9e7b30bbc6bc34ad4f7
arwebit <arwebcs@gmail.com>
2023-01-14 19:08:47 +0530

    Complete

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class LoginController extends Controller
 {
     //
+    private $rules = [];
+    private $messages = [];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+
+        $this->rules = [
+            "username" => "required",
+            "userpass" => "required"
+        ];
+        $this->messages = [
+            "username.required" => "Username is required",
+            "userpass.required" => "Password is required"
+        ];
+    }
     public function login(Request $req)
+    {
+        $user = "Admin";
+        $pass = "pass";
+        $validator = Validator::make($req->all(), $this->rules, $this->messages);
+        if (!$validator->fails()) {
+            $req->session()->put("session_username", $req->input("username"));
+            return redirect('home');
+        } else {
+            return back()->withErrors($validator->errors())->withInput();
+        }
+    }
+
+
+
+    /*public function login(Request $req)
     {
         $user = "Admin";
         $pass = "pass";
@@ -23,5 +59,5 @@ class LoginController extends Controller
         } else {
             return view('login', ["datas" => $req->input()]);
         }
-    }
+    }*/
 }
